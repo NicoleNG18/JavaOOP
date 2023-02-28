86335215ddc2475c1a3837732c068c8fc288c55f
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-02-28 21:39:37 +0200

    Abstraction / Card suit

---
+++ src/com/company/abstraction/hotelReservation/Season.java
@@ -0,0 +1,21 @@
+package com.company.abstraction.hotelReservation;
+
+public enum Season {
+
+    Spring(2),
+    Summer(4),
+    Autumn(1),
+    Winter(3);
+
+    private int multiplier;
+
+    Season(int multiplier){
+        this.multiplier=multiplier;
+    }
+
+    public int getMultiplier() {
+        return multiplier;
+    }
+
+}
+
