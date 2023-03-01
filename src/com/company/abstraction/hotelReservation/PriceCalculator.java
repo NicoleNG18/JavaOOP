0a1a0af74148c46112f1faaf85441fc55efc6c56
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-03-01 09:50:24 +0200

    Abstraction / Card suit

---
+++ src/com/company/abstraction/hotelReservation/PriceCalculator.java
@@ -0,0 +1,22 @@
+package com.company.abstraction.hotelReservation;
+
+public class PriceCalculator {
+
+    private double pricePerDay;
+    private int numberOfDays;
+    private Season season;
+    private DiscountType discount;
+
+    public PriceCalculator(double pricePerDay, int numberOfDays, Season season, DiscountType discount) {
+        this.pricePerDay = pricePerDay;
+        this.numberOfDays = numberOfDays;
+        this.season = season;
+        this.discount = discount;
+    }
+
+    public double returnThePrice() {
+        return pricePerDay * numberOfDays * season.getMultiplier() * discount.getDiscount();
+    }
+
+}
+
