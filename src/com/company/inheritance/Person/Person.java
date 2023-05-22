8051fbaa68eb58ddc6fe8b50b026a9987a179b83
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-05-22 20:43:08 +0300

    Inheritance / Person

---
+++ src/com/company/inheritance/Person/Person.java
@@ -0,0 +1,20 @@
+package com.company.inheritance.Person;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
