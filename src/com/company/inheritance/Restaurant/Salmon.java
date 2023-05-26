12ee61873dbe6827a38cdb6a9857181d61b9fa94
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-05-26 17:14:17 +0300

    Inheritance / Restaurant

---
+++ src/com/company/inheritance/Restaurant/Salmon.java
@@ -0,0 +1,12 @@
+package com.company.inheritance.Restaurant;
+
+import java.math.BigDecimal;
+
+public class Salmon extends MainDish{
+    final static double SALMON_GRAMS = 22;
+
+    public Salmon(String name, BigDecimal price) {
+        super(name, price, SALMON_GRAMS);
+    }
+}
+
