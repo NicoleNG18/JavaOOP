ded9a13288ba2f09844109511e8824927c6611bf
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-05-26 17:15:20 +0300

    Inheritance / Restaurant

---
+++ src/com/company/inheritance/Restaurant/Soup.java
@@ -0,0 +1,10 @@
+package com.company.inheritance.Restaurant;
+
+import java.math.BigDecimal;
+
+public class Soup extends Starter{
+    public Soup(String name, BigDecimal price, double grams) {
+        super(name, price, grams);
+    }
+}
+
