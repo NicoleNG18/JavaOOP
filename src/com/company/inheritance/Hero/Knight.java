d248643e87a8f96125c1f71903d9d6fb75367aaa
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-05-29 21:55:57 +0300

    Inheritance / Hero

---
+++ src/com/company/inheritance/Hero/Knight.java
@@ -0,0 +1,9 @@
+package com.company.inheritance.Hero;
+
+public class Knight extends Hero{
+
+    public Knight(String username, int level) {
+        super(username, level);
+    }
+}
+
---
+++ src/com/company/inheritance/Hero/Wizard.java
@@ -0,0 +1,9 @@
+package com.company.inheritance.Hero;
+
+public class Wizard extends Hero{
+
+    public Wizard(String username, int level) {
+        super(username, level);
+    }
+}
+
