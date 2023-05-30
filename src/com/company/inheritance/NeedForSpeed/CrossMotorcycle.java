7f7aaa6b8182c6c56582d490c7f955991923ee6c
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-05-30 20:36:37 +0300

    Inheritance / Need For Speed

---
+++ src/com/company/inheritance/NeedForSpeed/CrossMotorcycle.java
@@ -0,0 +1,9 @@
+package com.company.inheritance.NeedForSpeed;
+
+public class CrossMotorcycle extends Motorcycle {
+
+    public CrossMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
+
---
+++ src/com/company/inheritance/NeedForSpeed/FamilyCar.java
@@ -0,0 +1,8 @@
+package com.company.inheritance.NeedForSpeed;
+
+public class FamilyCar extends Car{
+
+    public FamilyCar(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
