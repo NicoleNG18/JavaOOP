93db28f4c57678dff010c6b837e880099cf68df3
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-06-10 15:08:13 +0300

    Polymorphism / WildFarm

---
+++ src/com/company/polymorphism/WildFarm/Animal.java
@@ -0,0 +1,56 @@
+package com.company.polymorphism.WildFarm;
+
+import java.text.DecimalFormat;
+
+public abstract class Animal {
+    protected static final DecimalFormat df = new DecimalFormat("#.##");
+    private String animalName;
+    private String animalType;
+    private Double animalWeight;
+    private Integer foodEaten;
+
+    public Animal(String animalName, String animalType, Double animalWeight) {
+        setAnimalName(animalName);
+        setAnimalType(animalType);
+        setAnimalWeight(animalWeight);
+        this.foodEaten = 0;
+    }
+
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    public void setAnimalType(String animalType) {
+        this.animalType = animalType;
+    }
+
+    public Double getAnimalWeight() {
+        return animalWeight;
+    }
+
+    public void setAnimalWeight(Double animalWeight) {
+        this.animalWeight = animalWeight;
+    }
+
+    public Integer getFoodEaten() {
+        return foodEaten;
+    }
+
+    public void setFoodEaten(Integer foodEaten) {
+        this.foodEaten = foodEaten;
+    }
+
+    public abstract void makeSound();
+
+    public abstract void eat(Food food);
+
+}
+
---
+++ src/com/company/polymorphism/WildFarm/Felime.java
@@ -0,0 +1,10 @@
+package com.company.polymorphism.WildFarm;
+
+public abstract class Felime extends Mammal{
+
+    public Felime(String animalName, String animalType, Double animalWeight,  String livingRegion) {
+        super(animalName, animalType, animalWeight,  livingRegion);
+    }
+
+}
+
