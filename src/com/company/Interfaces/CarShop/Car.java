9199e38076fe932ae5653a7da9b854d1d09cd43a
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-06-24 13:07:04 +0300

    Interfaces / Car shop

---
+++ src/com/company/Interfaces/CarShop/Car.java
@@ -0,0 +1,14 @@
+package com.company.Interfaces.CarShop;
+
+public interface Car  {
+    int TIRE = 4;
+
+    String getModel();
+
+    String getColor();
+
+    Integer getHorsePower();
+
+    String countryProduced();
+}
+
