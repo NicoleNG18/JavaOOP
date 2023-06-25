7b5f9da12c5e976b2bf95046a72d3011e3d91532
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-06-25 23:05:25 +0300

    Interfaces / Say hello

---
+++ src/com/company/Interfaces/SayHello/European.java
@@ -0,0 +1,15 @@
+package com.company.Interfaces.SayHello;
+
+public class European extends BasePerson {
+
+    public European(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Hello";
+    }
+
+}
+
---
+++ src/com/company/Interfaces/SayHello/Person.java
@@ -0,0 +1,11 @@
+package com.company.Interfaces.SayHello;
+
+public interface Person {
+
+    String sayHello();
+
+    String getName();
+
+}
+
+
