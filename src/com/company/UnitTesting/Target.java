3b5c4f60b8336fadd54b8a262f7c59de2eb06263
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-07-13 14:52:20 +0300

    Unit testing / Lab

---
+++ src/com/company/UnitTesting/Target.java
@@ -0,0 +1,12 @@
+package com.company.UnitTesting;
+
+public interface Target {
+    public int getHealth();
+
+    public void takeAttack(int attackPoints);
+
+    public int giveExperience();
+
+    public boolean isDead();
+}
+
---
+++ src/com/company/UnitTesting/Weapon.java
@@ -0,0 +1,10 @@
+package com.company.UnitTesting;
+
+public interface Weapon {
+    public int getAttackPoints();
+
+    public int getDurabilityPoints();
+
+    public void attack(Target target);
+}
+
