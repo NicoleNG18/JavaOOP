8da1c8a0520c743f645332b953880f0dd059951e
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-07-25 18:27:08 +0300

    High Quality Structure task exam

---
+++ src/com/company/HighQualityStructureExam01/Command.java
@@ -0,0 +1,14 @@
+package com.company.HighQualityStructureExam01;
+
+public enum Command {
+    AddField,
+    DeliverySupplement,
+    SupplementForField,
+    AddPlayer,
+    DragPlayer,
+    CalculateStrength,
+    GetStatistics,
+    Exit
+}
+
+
